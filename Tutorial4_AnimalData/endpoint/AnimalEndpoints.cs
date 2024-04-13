8d7ffcd88419dff72a3579c13dfb19773191d0ba
Maryna Kamienieva <mkdirwd@gmail.com>
2024-04-13 18:54:08 +0200

    the same methods but with MinimalAPI

--- Tutorial4_AnimalData/endpoint/AnimalEndpoints.cs
+++ Tutorial4_AnimalData/endpoint/AnimalEndpoints.cs
@@ -5,54 +5,99 @@ namespace Tutorial4_AnimalData.endpoint;
 
 public static class AnimalEndpoints
 {
-    public static void MapAnimalsEndpoint(this WebApplication app)
+    public static void MapAnimalsEndpoint(this WebApplication app, MockDb mockDb)
     {
-        app.MapGet("/animals", () =>
-        {
-            //process data
-            return Results.Ok();
-        });
-// 200 - Ok
-// 201 - Created
-        app.MapPost("/animals-minimal/{id}", (Animal animal) =>
-        {
-            Console.WriteLine(animal.id);
-            Console.WriteLine(animal.name);
 
-            return Results.Created();
-        });
-        // retrieve a specific animal by id
-        app.MapGet("/api/animals/{id:int}", (int id) =>
-            {
-                var animals = new MockDb().Animals;
-                animals.FirstOrDefault(s => s.id == id);
-                return animals == null ? Results.NotFound($"Animal with id {id} was not found") : Results.Ok(animals);
-            })
+        app.MapGet("/api/animals", () => Results.Ok(mockDb))
             .WithName("GetAnimals")
             .WithOpenApi();
-        //add an animal
+// 200 - Ok
+// 201 - Created
+        // app.MapPost("/animals-minimal/{id}", (Animal animal) =>
+        // {
+        //     Console.WriteLine(animal.id);
+        //     Console.WriteLine(animal.name);
+        //
+        //     return Results.Created();
+        // });
+        app.MapGet("/animals-minimal/{id:int}", (int id) =>
+            {
+                var animal = mockDb.Animals.FirstOrDefault(s => s.id == id);
+                return animal == null ? Results.NotFound($"Animal with id {id} was not found") : Results.Ok(animal);
+            })
+            .WithName("GetAnimal")
+            .WithOpenApi();
+        // retrieve a specific animal by id
+        // app.MapGet("/api/animals/{id:int}", (int id) =>
+        //     {
+        //         var animals = new MockDb().Animals;
+        //         animals.FirstOrDefault(s => s.id == id);
+        //         return animals == null ? Results.NotFound($"Animal with id {id} was not found") : Results.Ok(animals);
+        //     })
+        //     .WithName("GetAnimals")
+        //     .WithOpenApi();
         app.MapPost("/api/animals", (Animal animal) =>
             {
-                Animals.Add(animal);
+                mockDb.Animals.Add(animal);
                 return Results.StatusCode(StatusCodes.Status201Created);
             })
-            .WithName("AddStudent")
+            .WithName("CreateAnimal")
             .WithOpenApi();
         
+// app.MapGet("/api/students", () => Results.Ok(_students))
+//     .WithName("GetStudents")
+//     .WithOpenApi();
+//
+// app.MapGet("/api/students/{id:int}", (int id) =>
+//     {
+//         var student = _students.FirstOrDefault(s => s.IdStudent == id);
+//         return student == null ? Results.NotFound($"Student with id {id} was not found") : Results.Ok(student);
+//     })
+//     .WithName("GetStudent")
+//     .WithOpenApi();
+        app.MapPut("/api/animals/{id:int}", (int id, Animal animal) =>
+            {
+                var animalToEdit = mockDb.Animals.FirstOrDefault(s => s.id == id);
+                if (animalToEdit == null)
+                {
+                    return Results.NotFound($"Student with id {id} was not found");
+                }
+                mockDb.Animals.Remove(animalToEdit);
+                mockDb.Animals.Add(animal);
+                return Results.NoContent();
+            })
+            .WithName("UpdateAnimal")
+            .WithOpenApi();
         
         app.MapDelete("/api/animals/{id:int}", (int id) =>
             {
-                var studentToDelete = _students.FirstOrDefault(s => s.IdStudent == id);
-                if (studentToDelete == null)
+                var animalToDelete = mockDb.Animals.FirstOrDefault(s => s.id == id);
+                if (animalToDelete == null)
                 {
                     return Results.NoContent();
                 }
-                _students.Remove(studentToDelete);
+                mockDb.Animals.Remove(animalToDelete);
                 return Results.NoContent();
             })
-            .WithName("DeleteStudent")
+            .WithName("DeleteAnimal")
             .WithOpenApi();
-
+        
+        app.MapGet("{id:int}/visits", (int id)=>
+        {
+            var visitToAnimal = mockDb.Visits.FirstOrDefault(a => a.id == id);
+            return Results.Ok(visitToAnimal);
+        })
+        .WithName("GetVisits")
+        .WithOpenApi();
+        
+        app.MapPost("{id:int}/visits", (Visit visit) =>
+        {
+            mockDb.Visits.Add(visit);
+            return Results.StatusCode(StatusCodes.Status201Created);
+        })
+        .WithName("GetVisit")
+        .WithOpenApi();
+        
         app.Run();
     }
 }
