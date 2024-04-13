aae1179b863da9fcd7981d9449cc88e23e808c56
Maryna Kamienieva <mkdirwd@gmail.com>
2024-04-13 18:54:47 +0200

    add values for Visits list in MockDb

--- Tutorial4_AnimalData/database/MockDb.cs
+++ Tutorial4_AnimalData/database/MockDb.cs
@@ -15,6 +15,9 @@ public class MockDb
         Animals.Add((new Animal{id = 3, name = "Lizi", category = "cat", wheight = 8, fur_color = "orange"}));
         Animals.Add((new Animal{id = 4, name = "Jasper", category = "dog", wheight = 15, fur_color = "black"}));
         Animals.Add((new Animal{id = 5, name = "Lusi", category = "dog", wheight = 10, fur_color = "grey"}));
+        
+        Visits.Add((new Visit{id_visit = 1, date_of_visit = DateTime.Parse("2024-04-15 13:30:00") , animal = "dog", visit_description = "bandage the paw", price = 100}));
+        Visits.Add((new Visit{id_visit = 1, date_of_visit = DateTime.Parse("2024-04-16 17:15:00"), animal = "cat", visit_description = "routine inspection", price = 50}));
 
     }
 }
